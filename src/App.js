0ae3bd4857d27af5308a079649822fcb374afb02
DhruvSaboo <saboodhruv789@gmail.com>
2024-03-05 23:48:02 +0530

    udated data

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ const App = () => {
   return(
     <div className = "flex flex-col w-[100vw] h-[100vh] justify-center items-center bg-gray-200 overflow-x-hidden">
         <div className = "text-center">
-          <h1 className = "text-4xl font-bold">Our Testimonials</h1>
+          <h1 className = "text-4xl font-bold">My Guru's</h1>
       
-          <div className = "bg-violet-400 h-[4px] w-1/5 mt-1 mx-auto"></div>
+          <div className = "bg-violet-400 h-[4px] w-1/5 mt-3 mx-auto"></div>
           <Testimonial reviews = {reviews}></Testimonial>
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,38 +1,39 @@
 const reviews = [
   {
     id: 1,
-    name: "Pranay Gupta",
+    name: "Love Babbar",
     job: "Software Engineer",
-    image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
-    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+    image: "https://media.licdn.com/dms/image/C4D22AQFmabb-8L1kHw/feedshare-shrink_800/0/1666147381279?e=2147483647&v=beta&t=34DqBcWGLWV0BqdoNRGhoo6lQdx9wS3HtAGyTEGS9DM",
+    text: "Love Babbar is a Software Engineer and a Youtuber, primarily known for his Coding and Software Engineering skills. He is quite a popular figure among students as well as working professionals on various social media platforms, his YouTube and LinkedIn profiles amassing almost 1M subscribers. Henalso worked in Amazon and Microsoft."
+
   },
   {
     id: 2,
-    name: "Abir Pal",
-    job: "Graphic Designer",
-    image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
-    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+    name: "Lakshay Kumar",
+    job: "Software Engineer",
+    image: "https://media.licdn.com/dms/image/D4D03AQEMWkrlClEqaA/profile-displayphoto-shrink_800_800/0/1689785089442?e=2147483647&v=beta&t=UfFT94XqQ_vYlyEMEgICiXs7V74IEZzQr8KLyuhJTTM",
+    text: "Lakshay Kumar is an ace software engineer working in the role of Computer Scientist at Adobe Systems and a popular computer science instructor on CodeHelp Youtube. He is working in the industry for the past 4.5 years, working on different real-world problems. He is well-known among students for his amazingly simplified explanations with real-life examples, enabling students to understand complex topics very easily."
   },
   {
     id: 3,
-    name: "Soumya Banerjee",
-    job: "Marketing Manager",
-    image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
-    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+    name: "Parikh Jain",
+    job: "Founder, ProPeers",
+    image: "https://www.propeers.in/_next/image?url=%2Fimages%2Fparikh-jain.webp&w=828&q=100",
+    text: "Parikh Jain, a former mentor at Coding Ninjas and software engineer at Amazon, now dedicates himself to guiding students through his platform, Propeers. With a rich background in tech and a passion for education, he continues to empower aspiring professionals in their academic and career endeavors.",
   },
   {
     id: 4,
-    name: "Saikat Mukherjee",
-    job: "Content Strategist",
-    image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
-    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+    name: "Nishant Chahar",
+    job: "Software Developer",
+    image: "https://assets.telegraphindia.com/telegraph/2022/Aug/1661237688_unnamed-5.jpg",
+    text: "Nishant Chahar, currently employed as a Software Development Engineer at Microsoft, extends his expertise beyond his corporate role by mentoring and creating software-related content on YouTube. Renowned for his exceptional skills and engaging content, Nishant Chahar is a valuable asset to both the tech industry and the online community.",
   },
   {
     id: 5,
-    name: "Aritra Biswas",
-    job: "Data Analyst",
-    image: "https://avatars.githubusercontent.com/u/93366359?v=4",
-    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+    name: "Code With Harry",
+    job: "Great Teacher",
+    image: "https://yt3.googleusercontent.com/ytc/AIdro_krnyU9zev1u94JYs4opG8p1sYE3HQ9oButitIb7A=s900-c-k-c0x00ffffff-no-rj",
+    text: "Code with Harry is a popular YouTube channel and online platform created by Harry Bhai, offering high-quality tutorials and courses on various programming languages, web development, software engineering, and related topics. With millions of subscribers, Harry Bhai has gained recognition for his clear explanations, practical examples, and engaging teaching styles",
   },
 ];
 
